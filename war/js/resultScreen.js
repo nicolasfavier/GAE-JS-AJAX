0eff482f5b1ff099582eb326ff8356748aa88521
nicolasfavier <niko.favier@gmail.com>
2014-12-16 15:28:35 +0100

    affichage

--- war/js/resultScreen.js
+++ war/js/resultScreen.js
@@ -15,12 +15,24 @@ $(document).ready(function() {
 		$.get("searchbyword", {
 			search : search
 		}, function(data, status) {
-			alert("json " + data);
+			//alert("json " + data);
 			var obj = jQuery.parseJSON(data);
 
 			$.each(obj.listTrainning, function(key, value) {
 				addTrainningInHTML(value);
 			});
+			
+			$.each(obj.ListExercice, function(key, value) {
+				addExInHTML(value);
+			});
+			alert("ici");
+			
+			alert(obj.fluxrss.responseData);
+			$.each(obj.fluxrss.responseData.feed.entries, function(key, value) {
+				alert("news");
+				addNewsInHTML(value);
+			});
+			
 		});
 	}
 
@@ -35,5 +47,23 @@ function addTrainningInHTML(trainning) {
 							+ '</button></div><div class=" col-md-6 col-sm-6 col-xs-6"><label class="btn"> <span class="glyphicon glyphicon-time"></span> '
 							+ trainning.expectedTime + ' min.'
 							+ '</label></div>');
+
+}
+
+function addExInHTML(exercices) {
+	$("#exercicesList")
+			.append(
+					'<div class=" col-md-6 col-sm-6 col-xs-6">'
+							+ '<button type="submit" class="btn btn-link">'
+							+ exercices.title
+							+ '</button></div><div class=" col-md-6 col-sm-6 col-xs-6"><label class="btn"> <span class="glyphicon glyphicon-time"></span> '
+							+ exercices.durration + ' min.'
+							+ '</label></div>');
+
+}
+
+function addNewsInHTML(news) {
+	$("#newsList")
+			.append('<p>'+news.title+' : ' + news.content +" (edit by: "+ news.author+")"+'</p>');
 
 }
